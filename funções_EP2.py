88e57734d1ef5289c78f8d2eadafb94d9c6d3483
Thomas Chabro <thomascc@al.insper.edu.br>
2021-04-23 23:54:36 -0300

    Adicionou função para criar o baralho

---
+++ funções_EP2.py
@@ -0,0 +1,19 @@
+# Função para criar o baralho do jogo
+def cria_baralho ():
+    # Definindo presets para criação do baralho
+    baralho = []
+    naipes = ['♠', '♥', '♦', '♣']
+    # Adicionando todas as cartas de cada nipe
+    for i in naipes:
+        for el in range(2, 11):
+            carta = str(el) + i
+            baralho.append(carta)
+        carta = 'J' + i
+        baralho.append(carta)
+        carta = 'Q' + i
+        baralho.append(carta)
+        carta = 'K' + i
+        baralho.append(carta)
+        carta = 'A' + i
+        baralho.append(carta)
+    return baralho
